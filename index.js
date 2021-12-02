ab59e619398b224a55169c067bf9ddf23ecfe6f8
swamikl <swamikl@gmail.com>
2021-12-02 09:22:49 -0800

    fixed small issue

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 // to build the api 
 const express = require('express');
+const app = express();
+
 // middlewhere to help with parsing json 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const app = express();
 const db = require('./db');
 
 let port = process.env.PORT || 3000;
